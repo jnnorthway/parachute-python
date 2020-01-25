8215bb4eba8a3222c88b4df023187107a9b6f6af
James <jn.northway@gmail.com>
2020-01-25 14:13:39 -0800

    Add pre-commit check
    Adds pre-commit to repo.
    
    Change-Id: I884a37b2d968def058c0f49c5e4d6b85e0489b56
    Resolves: #6

--- tcpTest/server/tcpServer.py
+++ tcpTest/server/tcpServer.py
@@ -1,8 +1,9 @@
 import os
 import sys
-import socket
-sys.path.append(os.path.abspath(".."))
+
 from tcpTools import tcpServer
+
+sys.path.append(os.path.abspath(".."))
 
 
 def tcpServerTest():
@@ -19,16 +20,16 @@ def tcpServerSimpleTest():
     server = tcpServer()
     # Create a TCP socket at client side
     server.createUpdSocket()
-    server.TCPSocket.bind(server.server_data['address'])
-    print("Server bound on: %s:%s" % server.server_data['address'])
-    while(True):
-      data = server.recieveData()[0]
-      if data == server.EOF_MSG:
-        break
-      print('received: %s' % server.decode(data))
+    server.TCPSocket.bind(server.server_data["address"])
+    print("Server bound on: %s:%s" % server.server_data["address"])
+    while True:
+        data = server.recieveData()[0]
+        if data == server.EOF_MSG:
+            break
+        print("received: %s" % server.decode(data))
     server.close()
 
 
-if __name__== "__main__":
-  tcpServerTest()
-  # tcpServerSimpleTest()
+if __name__ == "__main__":
+    tcpServerTest()
+    # tcpServerSimpleTest()
--- udpTest/udpTools.py
+++ udpTest/udpTools.py
@@ -1,22 +1,22 @@
 import os
+import socket
 import sys
 import time
-import socket
 
 
-class udpTools():
+class udpTools:
     def __init__(self):
-        self.ACK_MSG = b'<ACK>'
-        self.EOF_MSG = b'<EOF>'
+        self.ACK_MSG = b"<ACK>"
+        self.EOF_MSG = b"<EOF>"
         self.timeout = 3.0
         self.max_attempts = 3
-        self.encoding = 'utf-8'
+        self.encoding = "utf-8"
         self.server_data = {
             # "address": ("192.168.56.1", 20001),
             # "address": ("127.0.0.1", 20001),
             "address": ("10.0.0.3", 20001),
             # "buffer": 8192
-            "buffer": 1024
+            "buffer": 1024,
         }
         self.UDPSocket = None
         self.file = None
@@ -25,42 +25,40 @@ class udpTools():
         self.msg_sent = 0
         self.msg_received = 0
 
-
     def printProgress(self):
         progress_str = "\rProgress: [%s] %s"
-        columns = int(os.popen('stty size', 'r').read().split()[1])
+        columns = int(os.popen("stty size", "r").read().split()[1])
         columns -= len(progress_str)
-        size_received = self.msg_received * self.server_data['buffer']
+        size_received = self.msg_received * self.server_data["buffer"]
         percentage = min(1, size_received / self.file_size)
-        percent_buffer = ''
+        percent_buffer = ""
         for i in range(columns):
             if (percentage * columns) >= i:
-                percent_buffer += '#'
+                percent_buffer += "#"
             else:
-                percent_buffer += ' '
-        format_percent = "{0:.0%}".format(percentage)
+                percent_buffer += " "
+        format_percent = f"{percentage:.0%}"
         sys.stdout.write(progress_str % (percent_buffer, format_percent))
 
-
     def clearProgress(self):
-        columns = int(os.popen('stty size', 'r').read().split()[1])
-        buffer = '\r'
+        columns = int(os.popen("stty size", "r").read().split()[1])
+        buffer = "\r"
         for _ in range(columns):
-            buffer += ' '
-        buffer += '\r'
+            buffer += " "
+        buffer += "\r"
         sys.stdout.write(buffer)
 
     def createUpdSocket(self):
         if self.UDPSocket is None:
             print("Creating socket.")
-            self.UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
+            self.UDPSocket = socket.socket(
+                family=socket.AF_INET, type=socket.SOCK_DGRAM,
+            )
 
     def printFileInfo(self):
         self.fileInfo(self.file)
         print("file path: %s" % self.file)
         print("file size: %d bytes" % self.file_size)
-
 
     def fileInfo(self, file_path):
         self.file = file_path
@@ -69,24 +67,20 @@ class udpTools():
             self.file_stats = os.stat(self.file)
             self.file_size = self.file_stats.st_size
 
-
     def fileName(self):
         self.file_name = os.path.split(self.file)[1]
 
-    
     def recieveData(self):
         assert self.UDPSocket, "No socket available."
         try:
-            data = self.UDPSocket.recvfrom(self.server_data['buffer'])
-        except:
+            data = self.UDPSocket.recvfrom(self.server_data["buffer"])
+        except Exception:
             return None
         self.msg_received += 1
         return data
 
-
     def decode(self, data):
         return data.decode(self.encoding)
-
 
     def close(self):
         self.UDPSocket.close()
@@ -97,14 +91,12 @@ class updClient(udpTools):
         super().__init__()
         self.fileInfo(file_path)
 
-
     def sendRawData(self, data):
         assert self.UDPSocket, "No socket available."
         if isinstance(data, str):
             data = str.encode(data)
         assert isinstance(data, bytes), "data not in byte form."
-        self.UDPSocket.sendto(data, self.server_data['address'])
-
+        self.UDPSocket.sendto(data, self.server_data["address"])
 
     def sendData(self, data, attempt=0):
         assert self.UDPSocket, "No socket available."
@@ -112,18 +104,19 @@ class updClient(udpTools):
         if isinstance(data, str):
             data = str.encode(data)
         assert isinstance(data, bytes), "data not in byte form."
-        self.UDPSocket.sendto(data, self.server_data['address'])
+        self.UDPSocket.sendto(data, self.server_data["address"])
         msgFromServer = self.recieveData()
-        if msgFromServer == None:
+        if msgFromServer is None:
             attempt += 1
             self.clearProgress()
             print("Failed to receive, trying again: attempt = %d" % attempt)
             return self.sendData(data, attempt)
         msgFromServer = msgFromServer[0]
-        assert msgFromServer == self.ACK_MSG, "Something went wrong, \"%s\"" % self.decode(msgFromServer)
+        assert (
+            msgFromServer == self.ACK_MSG
+        ), 'Something went wrong, "%s"' % self.decode(msgFromServer)
         self.msg_sent += 1
         return msgFromServer
-
 
     def sendFile(self):
         start_time = time.time()
@@ -133,13 +126,13 @@ class updClient(udpTools):
         self.sendData(self.file_name)
         self.sendData(str(self.file_size).encode())
         f = open(self.file, "rb")
-        data = f.read(self.server_data['buffer'])
+        data = f.read(self.server_data["buffer"])
         while data:
             # Send to server using created UDP socket
             self.sendData(data)
             self.printProgress()
-            
-            data = f.read(self.server_data['buffer'])
+
+            data = f.read(self.server_data["buffer"])
         f.close()
         self.sendData(self.EOF_MSG)
         print("\nFile sent.")
@@ -148,9 +141,8 @@ class updClient(udpTools):
 
 class updServer(udpTools):
     def __init__(self):
-        self.resource_path = 'resources'
+        self.resource_path = "resources"
         super().__init__()
-
 
     def sendData(self, data, address):
         assert self.UDPSocket, "No socket available."
@@ -160,14 +152,13 @@ class updServer(udpTools):
         self.UDPSocket.sendto(data, address)
         self.msg_sent += 1
 
-
     def receiveFile(self):
         # Listen for incoming datagrams
         message = None
-        data = b''
-        self.UDPSocket.bind(self.server_data['address'])
-        print("Server bound on: %s:%s" % self.server_data['address'])
-        while(self.EOF_MSG != message):
+        data = b""
+        self.UDPSocket.bind(self.server_data["address"])
+        print("Server bound on: %s:%s" % self.server_data["address"])
+        while self.EOF_MSG != message:
             bytesAddressPair = self.recieveData()
             dpId = bytesAddressPair[0]
             bytesAddressPair = self.recieveData()
@@ -175,12 +166,12 @@ class updServer(udpTools):
             message = bytesAddressPair[0]
             address = bytesAddressPair[1]
             if int(dpId) != self.msg_sent:
-                print("\nclient id: %s != server id: %d" % (dpId, self.msg_sent))
+                print("\nclient id: %s != server id: %d" % (dpId, self.msg_sent),)
                 self.msg_sent -= 1
                 self.sendData(self.ACK_MSG, address)
                 continue
             if self.file is None:
-                self.file = os.path.join(self.resource_path, self.decode(message))
+                self.file = os.path.join(self.resource_path, self.decode(message),)
                 start_time = time.time()
                 print("receiving file: %s" % self.file)
                 start_time = time.time()
@@ -193,7 +184,7 @@ class updServer(udpTools):
             # Sending a reply to client
             self.sendData(self.ACK_MSG, address)
         data = data.strip(self.EOF_MSG)
-        f=open(self.file, "wb")
+        f = open(self.file, "wb")
         f.write(data)
         print("\nFile written: %s" % self.file)
         f.close()
